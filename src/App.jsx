9cd2c4e1b77f80a6f01138ea5c8e00f02a736183
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-03-14 15:26:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-2xl text-center text-blue-500'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, nobis vitae. Laboriosam cumque assumenda, inventore tempora commodi ducimus dolorem debitis ab? Ratione commodi itaque deleniti eveniet esse saepe nulla eaque?
+      </p>
+    </div>
+  )
+}
+
+export default App
