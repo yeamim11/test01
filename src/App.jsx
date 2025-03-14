d66989f0435caf415e604223176ece6e54b43e59
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-03-14 15:37:09 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Git Test</h1>
+      <h1 className='text-center text-5xl font-bold bg-black text-white'>Git Test</h1>
       <p className='text-2xl text-center text-blue-500'>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, nobis vitae. Laboriosam cumque assumenda, inventore tempora commodi ducimus dolorem debitis ab? Ratione commodi itaque deleniti eveniet esse saepe nulla eaque?
       </p>
