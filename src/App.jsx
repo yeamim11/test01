53298a535eb73541660c75c93a239b7b9e863de8
yeamim11 <165083549+yeamim11@users.noreply.github.com>
2025-03-14 15:33:01 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      <h1>Git Test</h1>
       <p className='text-2xl text-center text-blue-500'>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, nobis vitae. Laboriosam cumque assumenda, inventore tempora commodi ducimus dolorem debitis ab? Ratione commodi itaque deleniti eveniet esse saepe nulla eaque?
       </p>
