27411ba5e03f7c6f56abe3c731f7bf12a4f363be
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-06-25 22:44:30 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Test from './Component/Test/Test'
 
 const App = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center bg-gray-100'>
       <h1 className='text-center text-5xl font-bold bg-black text-white'>Git Test</h1>
       <p className='text-2xl text-center text-blue-500'>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, nobis vitae. Laboriosam cumque assumenda, inventore tempora commodi ducimus dolorem debitis ab? Ratione commodi itaque deleniti eveniet esse saepe nulla eaque?
       </p>
+      <Test/>
     </div>
   )
 }
---
+++ src/Component/Test/Test.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold text-center bg-amber-600 text-white mt-2.5'>Test</h1>
+        <p className='text-2xl text-center bg-gray-200 text-gray-800'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius nesciunt maxime accusamus cumque voluptas repellendus, quidem illum veniam ipsum! Facere quia illo quisquam incidunt voluptatum dolor ratione id magnam consectetur?
+        </p>
+    </div>
+  )
+}
+
+export default Test
