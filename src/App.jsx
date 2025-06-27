3ac02f60391b98967a3f3828d15ef6c6c0cda276
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-06-28 02:24:59 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Test from './Component/Test/Test'
 
 const App = () => {
   return (
-    <div className='flex flex-col items-center justify-center bg-gray-100'>
+    <div>
       <h1 className='text-center text-5xl font-bold bg-black text-white'>Git Test</h1>
       <p className='text-2xl text-center text-blue-500'>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, nobis vitae. Laboriosam cumque assumenda, inventore tempora commodi ducimus dolorem debitis ab? Ratione commodi itaque deleniti eveniet esse saepe nulla eaque?
---
+++ src/Component/Test2/Tset2.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Tset2 = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold text-center bg-blue-600 text-white mt-2.5'>
+                Career Objective
+            </h1>
+            <p>
+                As a BBS graduate in Accounting subject with a certified in Front-End Development.I use my technical skills to create engaging, user-centric web applications.
+                Making responsive, user-friendly interfaces that improve user experience and  organizational goals is something I'm very interested in.
+                A recent accounting BBS graduate is looking to use my business acumen  in a demanding position that fosters professional development and advances organizational success.
+            </p>
+        </div>
+    )
+}
+
+export default Tset2
