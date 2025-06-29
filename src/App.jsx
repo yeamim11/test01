bd2b52f847a2767286214d52361a3f24df4b4b20
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-06-29 14:53:03 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Test from './Component/Test/Test'
 import Tset2 from './Component/Test2/Tset2'
+import Videolink from './Component/Test2/Videolink'
 
 const App = () => {
   return (
-    <div>
+    <div className=''>
       <h1 className='text-center text-5xl font-bold bg-black text-white'>Git Test</h1>
       <p className='text-2xl text-center text-blue-500'>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, nobis vitae. Laboriosam cumque assumenda, inventore tempora commodi ducimus dolorem debitis ab? Ratione commodi itaque deleniti eveniet esse saepe nulla eaque?
       </p>
       <Test/>
       <Tset2/>
+      <Videolink/>
     </div>
   )
 }
---
+++ src/Component/Test2/Videolink.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Videolink = () => {
+    return (
+        <div className='flex flex-col items-center justify-center mt-2.5 p-5 gap-10 rounded-lg'>
+            <h1 className='font-bold text-center text-3xl bg-lime-600 to-white w-full'>Video Editor Vfx Video</h1>
+            <div className="">
+                <button className='bg-green-700 text-white p-3.5 rounded-full'><a href="https://drive.google.com/drive/folders/1IIZloB30E4QbZqFUFF604MkrZT8y0ruD">
+                link 1
+                </a></button>
+            </div>
+        </div>
+    )
+}
+
+export default Videolink
