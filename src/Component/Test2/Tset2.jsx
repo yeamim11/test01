6ff0fd0abbdee8944d934d62fddbfbea2a669ed0
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-02 15:47:46 +0600

    add

--- src/Component/Test2/Tset2.jsx
+++ src/Component/Test2/Tset2.jsx
@@ -11,6 +11,9 @@ const Tset2 = () => {
                 Making responsive, user-friendly interfaces that improve user experience and  organizational goals is something I'm very interested in.
                 A recent accounting BBS graduate is looking to use my business acumen  in a demanding position that fosters professional development and advances organizational success.
             </p>
+            <p className='text-2xl text-center bg-gray-500 text-gray-800'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab autem eligendi ipsam? Blanditiis magni atque numquam dolore quis? Facere perspiciatis sequi repellat nesciunt officiis exercitationem quis quod optio ratione doloribus corporis voluptates repudiandae nostrum, enim tempore voluptatem odit distinctio ea alias nemo maiores veritatis! Aliquid qui possimus temporibus, cupiditate adipisci tenetur amet tempore veritatis alias reprehenderit impedit assumenda tempora incidunt accusantium modi natus nisi blanditiis. Accusamus nulla quibusdam, temporibus officia excepturi odit voluptatem pariatur suscipit atque perferendis molestiae dignissimos obcaecati repellendus, quae ad sequi nesciunt debitis omnis! Eveniet possimus alias architecto, dolorum nostrum repellendus veritatis quidem ducimus mollitia perferendis! Atque.
+            </p>
         </div>
     )
 }
