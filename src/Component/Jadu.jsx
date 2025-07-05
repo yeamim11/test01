0c77ed22f0fd37146c65c6f7de83cd69e0fd8a8a
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-05 22:53:46 +0600

    asd

--- src/Component/Jadu.jsx
+++ src/Component/Jadu.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Jadu = () => {
   return (
-    <div  className='flex flex-col items-center justify-center min-h-screen bg-gray-50'>
-      <h1 className='text-3xl font-bold text-center text-blue-500 bg-gray-100 p-4'>
+    <div  className='flex flex-col items-center justify-center bg-gray-50'>
+      <h1 className='text-3xl font-bold text-center text-blue-500 bg-gray-700 w-full p-4'>
         jadu
       </h1>
       <p className='text-lg text-gray-700 p-4'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reiciendis dicta dolorum esse impedit? At, adipisci! Libero corrupti accusantium veniam, tempore soluta voluptates placeat possimus odit, quo inventore a? At?
+      </p>
+      <p className='text-lg text-gray-700 p-4'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem non odio natus odit earum veniam ducimus qui pariatur iste inventore placeat laudantium corporis, beatae iure aliquid error vero deserunt rerum.
       </p>
     </div>
   )
