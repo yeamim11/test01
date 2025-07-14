b852eaa0d7efe724d2ded17f73d764333d1c1eef
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-14 21:56:46 +0600

    asd

--- src/Component/from.jsx
+++ src/Component/from.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const from = () => {
   return (
-    <div>from</div>
+    <div>
+      <h1>komffe</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur laudantium earum. Repudiandae quibusdam, eaque delectus, numquam asperiores, rerum pariatur quo sapiente nobis rem odit ducimus fuga possimus quod cumque!
+      </p>
+    </div>
   )
 }
 
