1c1eab0c2369893a17572f743e55962915db8554
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-07-16 23:58:44 +0600

    add

--- src/Component/from.jsx
+++ src/Component/from.jsx
@@ -7,6 +7,9 @@ const from = () => {
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur laudantium earum. Repudiandae quibusdam, eaque delectus, numquam asperiores, rerum pariatur quo sapiente nobis rem odit ducimus fuga possimus quod cumque!
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque nesciunt laudantium, eum facilis aspernatur, ipsa temporibus accusantium nulla natus voluptas asperiores deleniti quam velit voluptatem consequatur maxime dolor fugiat voluptate?
+      </p>
     </div>
   )
 }
